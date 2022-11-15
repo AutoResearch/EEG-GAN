59da9f888f159d9481a42cdf8c61b3fef97aacdf
chadcwilliams <ccwillia@uvic.ca>
2022-11-15 14:13:06 -0800

    Updated method

--- classification_script/gansEEG_ExtractSampleSizeData.py
+++ classification_script/gansEEG_ExtractSampleSizeData.py
@@ -1,23 +1,40 @@
+#Import Modules
 import numpy as np
 import random as rnd
 
-EEGData = np.genfromtxt('ganTrialERP_len100.csv', delimiter=',', skip_header=1)
+#Set seeds for reproducibility
+np.random.seed(1251)
+rnd.seed(1251)
+
+#Load EEG data
+EEGData = np.genfromtxt('data/ganTrialERP_len100.csv', delimiter=',', skip_header=1)
+EEGDataHeader = np.genfromtxt('data/ganTrialERP_len100.csv', delimiter=',', names=True).dtype.names
+
+#Create test file of 400 participants
 testParticipants = np.sort(rnd.sample(range(1,int(np.unique(EEGData[:,0])[-1])+1), 400))
-
 testEEGData = []
-for participant in testParticipants:
+for participant in testParticipants: 
+    #Add to test dataset
     testEEGData.extend(EEGData[EEGData[:,0]==participant,:])
-    EEGData = np.delete(EEGData, np.where(EEGData[:,0]==participant), axis=0)  
+    
+    #Remove from dataset to be used for training
+    EEGData = np.delete(EEGData, np.where(EEGData[:,0]==participant), axis=0)
 
-saveFilename = 'Reduced/gansTrialERP_len100_TestSS400.csv'
-np.savetxt(saveFilename, testEEGData, delimiter=",")
+#Save File
+saveFilename = 'data/training_data/gansTrialERP_len100_TestSS400.csv'
+np.savetxt(saveFilename, testEEGData, delimiter=",", fmt='%f', header=','.join(EEGDataHeader), comments='')
     
+#Iterate through different sample sizes to save data
 for sampleSize in np.arange(5,101,5):
-    sampleParticipants = rnd.sample(range(1,int(np.unique(EEGData[:,0])[-1])+1), sampleSize)
+
+    #Determine samples of current sample size
+    sampleParticipants = np.sort(rnd.sample(list(np.unique(EEGData[:,0])), sampleSize))
     
+    #Create new data frame with determined samples
     reducedData = []
     for participant in sampleParticipants:
         reducedData.extend(EEGData[EEGData[:,0]==participant,:])
 
-    saveFilename = 'Reduced/gansTrialERP_len100_SS' + str(sampleSize).zfill(3) + '.csv'
-    np.savetxt(saveFilename, reducedData, delimiter=",")
+    #Save file
+    saveFilename = 'data/training_data/gansTrialERP_len100_SS' + str(sampleSize).zfill(3) + '.csv'
+    np.savetxt(saveFilename, reducedData, delimiter=",", fmt='%f', header=','.join(EEGDataHeader), comments='')
