cd3937a451c4a64765db83cbe46c0a3c19a12815
why_hardt <111064762+whyhardt@users.noreply.github.com>
2023-04-28 17:46:40 +0200

    updated transformer ae

--- nn_architecture/transformer_autoencoder.py
+++ nn_architecture/transformer_autoencoder.py
@@ -1,10 +1,13 @@
+import os
+from typing import Optional
+
+import pandas as pd
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader, Dataset
 
 
 class TransformerAutoencoder(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim, num_layers, dropout):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers, dropout, **kwargs):
         super(TransformerAutoencoder, self).__init__()
 
         self.encoder_layer = nn.TransformerEncoderLayer(d_model=input_dim, nhead=5, dim_feedforward=hidden_dim,
@@ -18,6 +21,8 @@ class TransformerAutoencoder(nn.Module):
         self.linear_enc = nn.Linear(input_dim, output_dim)
 
         self.linear_dec = nn.Linear(output_dim, input_dim)
+
+
 
     def forward(self, data):
         x = self.encoder(data)
@@ -36,6 +41,10 @@ class TransformerAutoencoder(nn.Module):
         # x = self.decoder(target, x)
         return x
 
+    def save(self, path):
+        path = 'trained_ae'
+        file = f'ae_{pd.Timestamp.now().strftime("%Y%m%d_%H%M%S")}.pth'
+        # torch.save(save, os.path.join(path, file))
 
 def train_model(model, dataloader, optimizer, criterion):
     model.train()
@@ -63,13 +72,26 @@ def test_model(model, dataloader, criterion):
     return total_loss / len(dataloader)
 
 
-def train(num_epochs, model, train_dataloader, test_dataloader, optimizer, criterion):
-    train_losses = []
-    test_losses = []
-    for epoch in range(num_epochs):
-        train_loss = train_model(model, train_dataloader, optimizer, criterion)
-        test_loss = test_model(model, test_dataloader, criterion)
-        train_losses.append(train_loss)
-        test_losses.append(test_loss)
-        print(f"Epoch {epoch + 1}/{num_epochs}: train_loss={train_loss:.6f}, test_loss={test_loss:.6f}")
-    return train_losses, test_losses, model
+def train(num_epochs, model, train_dataloader, test_dataloader, optimizer, criterion, configuration: Optional[dict] = None):
+    try:
+        train_losses = []
+        test_losses = []
+        for epoch in range(num_epochs):
+            train_loss = train_model(model, train_dataloader, optimizer, criterion)
+            test_loss = test_model(model, test_dataloader, criterion)
+            train_losses.append(train_loss)
+            test_losses.append(test_loss)
+            print(f"Epoch {epoch + 1}/{num_epochs}: train_loss={train_loss:.6f}, test_loss={test_loss:.6f}")
+        return train_losses, test_losses, model
+    except KeyboardInterrupt:
+        # save model at KeyboardInterrupt
+        print("keyboard interrupt detected.")
+        if configuration is not None:
+            print("Configuration found.")
+            configuration["model"]["state_dict"] = model.state_dict()  # update model's state dict
+            save(configuration, configuration["general"]["default_save_path"])
+
+
+def save(configuration, path):
+    torch.save(configuration, path)
+    print("Saved model and configuration to " + path)
