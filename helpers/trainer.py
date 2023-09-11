3696d6a5f04c307aacda4c5462be3423251d007e
Chad C Williams <ccwillia@uvic.ca>
2023-09-11 05:59:32 -0700

    Added missed changes

--- helpers/trainer.py
+++ helpers/trainer.py
@@ -456,6 +456,8 @@ class AETrainer(Trainer):
             'input_dim': opt['input_dim'],
             'output_dim': opt['output_dim'],
             'output_dim_2': opt['output_dim_2'],
+            'num_layers': opt['num_layers'],
+            'num_heads': opt['num_heads'],
             'dataloader': {
                 'path_dataset': opt['path_dataset'] if 'path_dataset' in opt else None,
                 'col_label': opt['conditions'] if 'conditions' in opt else None,
