d5e0fc8e76f3b6f152cbc78caf2e806cfeae9210
Chad C Williams <ccwillia@uvic.ca>
2023-10-30 13:59:37 -0700

    Fix

--- helpers/trainer.py
+++ helpers/trainer.py
@@ -191,7 +191,7 @@ class GANTrainer(Trainer):
                 self.discriminator_scheduler.step(np.abs(d_loss_batch/i_batch))
                 if self.counterfactual_scheduler is not None:
                     for i in range(len(self.generator_optimizer.param_groups)):
-                        if self.generator_optimizer.param_groups[i]['lr'] < g_lr[i]: #Only update if the lr has been decreased
+                        if self.discriminator_optimizer.param_groups[i]['lr'] < d_lr[i]: #Only update if the lr has been decreased
                             new_g_lr = g_lr[i]+(g_lr[i]*self.counterfactual_scheduler*self.d_scheduler)
                             self.generator_optimizer.param_groups[i]['lr'] = new_g_lr
                             print(f"Epoch {str(epoch).zfill(5)}: increasing counterfactual learning rate of group {i} to {new_g_lr}")
@@ -199,7 +199,7 @@ class GANTrainer(Trainer):
                 self.generator_scheduler.step(np.abs(g_loss_batch/i_batch))
                 if self.counterfactual_scheduler is not None:
                     for i in range(len(self.discriminator_optimizer.param_groups)):
-                        if self.discriminator_optimizer.param_groups[i]['lr'] < d_lr[i]: #Only update if the lr has been decreased
+                        if self.generator_optimizer.param_groups[i]['lr'] < g_lr[i]: #Only update if the lr has been decreased
                             new_d_lr = d_lr[i]+(d_lr[i]*self.counterfactual_scheduler*self.g_scheduler)
                             self.discriminator_optimizer.param_groups[i]['lr'] = new_d_lr
                             print(f"Epoch {str(epoch).zfill(5)}: increasing counterfactual learning rate of group {i} to {new_d_lr}")
