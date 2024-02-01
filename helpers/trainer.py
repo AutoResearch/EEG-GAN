8404128a155407032b9ee9f3ef48fdcfb9bfc9eb
Chad C Williams <ccwillia@uvic.ca>
2024-02-01 10:02:20 -0800

    Cut data for AE decoding

--- helpers/trainer.py
+++ helpers/trainer.py
@@ -319,9 +319,9 @@ class GANTrainer(Trainer):
 
                 if decode_imgs:
                     if not hasattr(self.generator, 'module'):
-                        gen_samples = self.generator.decoder.decode(fake_data[:, :, :self.generator.channels].reshape(-1, self.generator.seq_len, self.generator.channels))
+                        gen_samples = self.generator.decoder.decode(fake_data[:, :-self.padding, :self.generator.channels].reshape(-1, self.generator.seq_len, self.generator.channels))
                     else:
-                        gen_samples = self.generator.module.decoder.decode(fake_data[:, :, :self.generator.module.channels].reshape(-1, self.generator.module.seq_len, self.generator.module.channels))
+                        gen_samples = self.generator.module.decoder.decode(fake_data[:, :-self.padding, :self.generator.module.channels].reshape(-1, self.generator.module.seq_len, self.generator.module.channels))
                     # concatenate gen_cond_data_orig with decoded fake_data
                     # currently redundant because gen_cond_data is None in this case
                     if self.input_sequence_length != 0 and self.input_sequence_length != self.sequence_length:
