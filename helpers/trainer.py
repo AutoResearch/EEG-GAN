79e5f9e8f3978390128190e3b99e7d7f3bb78b99
Chad C Williams <ccwillia@uvic.ca>
2024-02-01 10:47:38 -0800

    fix

--- helpers/trainer.py
+++ helpers/trainer.py
@@ -318,19 +318,15 @@ class GANTrainer(Trainer):
                     decode_imgs = isinstance(self.generator.module, DecoderGenerator) and not self.generator.module.decode
 
                 if decode_imgs:
-                    print(self.padding)
                     if not hasattr(self.generator, 'module'):
-                        gen_samples = self.generator.decoder.decode(fake_data[:, :-self.padding, :self.generator.channels].reshape(-1, self.generator.seq_len, self.generator.channels))
+                        fake_input = fake_data[:, :, :self.generator.channels].reshape(-1, self.generator.seq_len, self.generator.channels)
+                        fake_input = fake_input[:,:-self.padding,:] if self.padding > 0 else fake_input
+                        gen_samples = self.generator.decoder.decode(fake_input)
                     else:
-                        gen_samples = self.generator.module.decoder.decode(fake_data[:, :, :self.generator.module.channels].reshape(-1, self.generator.module.seq_len, self.generator.module.channels))
+                        fake_input = fake_data[:, :, :self.generator.module.channels].reshape(-1, self.generator.module.seq_len, self.generator.module.channels)
+                        fake_input = fake_input[:,:-self.padding,:] if self.padding > 0 else fake_input
+                        gen_samples = self.generator.module.decoder.decode(fake_input)
                     
-                    print('PAD')
-                    print(self.padding)
-                    print('GEN')
-                    print(gen_samples.shape)
-                    gen_samples = gen_samples[:,:-self.padding,:] if self.padding > 0 else gen_samples
-                    print('NEW GEN')
-                    print(gen_samples.shape)
                     # concatenate gen_cond_data_orig with decoded fake_data
                     # currently redundant because gen_cond_data is None in this case
                     if self.input_sequence_length != 0 and self.input_sequence_length != self.sequence_length:
