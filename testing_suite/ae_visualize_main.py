7847e04f9f574c0d7791429a12c6c9b726eb8b69
Chad C Williams <ccwillia@uvic.ca>
2024-02-08 08:57:02 -0800

    Updated testing suite

--- testing_suite/ae_visualize_main.py
+++ testing_suite/ae_visualize_main.py
@@ -3,45 +3,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 from torch.nn.modules.utils import consume_prefix_in_state_dict_if_present
+from tqdm import tqdm
 from nn_architecture.ae_networks import TransformerAutoencoder, TransformerDoubleAutoencoder, TransformerFlattenAutoencoder
 from helpers.dataloader import Dataloader
 
-#User input
-data_checkpoint = 'data/ganTrialElectrodeERP_p500_e18_len100.csv'
-ae_checkpoint = 'trained_ae/ae_ddp_4000ep_20230824_145643.pt'
+#### User input ####
+data_checkpoint = 'data/ganTrialElectrodeERP_p100_e2_len100.csv'
+ae_checkpoint = 'trained_ae/ae_ddp_500ep_20240207_220907.pt'
 
-#Load
-ae_dict = torch.load(ae_checkpoint, map_location=torch.device('cpu'))
+#### Load data ####
 dataloader = Dataloader(data_checkpoint, col_label='Condition', channel_label='Electrode')
-dataset = dataloader.get_data()
-sequence_length = dataset.shape[1] - dataloader.labels.shape[1]
-device = torch.device('cpu')
+dataset = dataloader.get_data().detach().numpy()
+norm = lambda data: (data-np.min(data)) / (np.max(data) - np.min(data))
+dataset = np.concatenate((dataset[:,[0],:], norm(dataset[:,1:,:])), axis=1)
 
-#Initiate
+#### Initiate autoencoder ####
+device = torch.device('cpu')
+ae_dict = torch.load(ae_checkpoint, map_location=device)
 if ae_dict['configuration']['target'] == 'channels':
     ae_dict['configuration']['target'] = TransformerAutoencoder.TARGET_CHANNELS
     autoencoder = TransformerAutoencoder(**ae_dict['configuration']).to(device)
 elif ae_dict['configuration']['target'] == 'time':
     ae_dict['configuration']['target'] = TransformerAutoencoder.TARGET_TIMESERIES
-    # switch values for output_dim and output_dim_2
-    ae_output_dim = ae_dict['configuration']['output_dim']
-    ae_dict['configuration']['output_dim'] = ae_dict['configuration']['output_dim_2']
-    ae_dict['configuration']['output_dim_2'] = ae_output_dim
     autoencoder = TransformerAutoencoder(**ae_dict['configuration']).to(device)
 elif ae_dict['configuration']['target'] == 'full':
-    autoencoder = TransformerDoubleAutoencoder(**ae_dict['configuration'], sequence_length=sequence_length, training_level=2).to(device)
-    autoencoder.model_1 = TransformerDoubleAutoencoder(**ae_dict['configuration'], sequence_length=sequence_length, training_level=1).to(device)
-    autoencoder.model_1.eval()
+    '''
+    autoencoder = TransformerDoubleAutoencoder(**ae_dict['configuration'], training_level=2).to(device)
+    autoencoder.model_1 = TransformerDoubleAutoencoder(**ae_dict['configuration'], training_level=1).to(device)
+    '''
+    ae_dict['configuration']['target'] = TransformerAutoencoder.TARGET_BOTH
+    autoencoder = TransformerDoubleAutoencoder(**ae_dict['configuration']).to(device)
 else:
     raise ValueError(f"Autoencoder class {ae_dict['configuration']['model_class']} not recognized.")
 consume_prefix_in_state_dict_if_present(ae_dict['model'], 'module.')
 autoencoder.load_state_dict(ae_dict['model'])
-# freeze the autoencoder
 for param in autoencoder.parameters():
     param.requires_grad = False
-autoencoder.eval()
 
-#Test
+
+#### Plot losses ####
 plt.figure()
 plt.plot(ae_dict['train_loss'], label='Train Loss')
 plt.plot(ae_dict['test_loss'], label = 'Test Loss')
@@ -50,11 +50,7 @@ plt.xlabel('Epoch')
 plt.legend()
 plt.show()
 
-def norm(data):
-    return (data-np.min(data)) / (np.max(data) - np.min(data))
-
-dataset = norm(dataset.detach().numpy())
-
+#### Plot trial level samples ####
 fig, axs = plt.subplots(5,1)
 for i in range(5):
     sample = np.random.choice(len(dataset), 1)
@@ -63,4 +59,26 @@ for i in range(5):
     axs[i].plot(autoencoder.decode(autoencoder.encode(torch.from_numpy(data)))[0,:,0].detach().numpy(), label='Reconstructed')
     axs[i].legend()
 plt.show()
+
+#### Plot encoded-decoded averages ####
+
+#Create reconstructed datasaet
+ae_dataset = np.empty(dataset.shape)
+print('Reconstructing dataset with the autoencoder...')
+for sample in tqdm(range(dataset.shape[0])):
+    data = dataset[[sample],1:,:]
+    ae_data = autoencoder.decode(autoencoder.encode(torch.from_numpy(data))).detach().numpy()
+    ae_dataset[sample,:,:] = np.concatenate((dataset[sample,0,:].reshape(1,1,-1), ae_data), axis=1)
+
+fig, ax = plt.subplots(2,dataset.shape[-1])
+for electrode in range(dataset.shape[-1]):
+    ax[0,electrode].plot(np.mean(dataset[dataset[:,0,0]==0,1:,electrode],axis=0))
+    ax[0,electrode].plot(np.mean(dataset[dataset[:,0,0]==1,1:,electrode],axis=0))
+
+    ax[1,electrode].plot(np.mean(ae_dataset[ae_dataset[:,0,0]==0,1:,electrode],axis=0))
+    ax[1,electrode].plot(np.mean(ae_dataset[ae_dataset[:,0,0]==1,1:,electrode],axis=0))
+plt.show()
+
+
+
 
