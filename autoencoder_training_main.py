8aa932f81dc160d6f92fdaba7dff443140035458
Chad C Williams <ccwillia@uvic.ca>
2024-02-08 08:30:48 -0800

    revert

--- autoencoder_training_main.py
+++ autoencoder_training_main.py
@@ -120,6 +120,7 @@ def main():
         raise FileNotFoundError(f"Checkpoint file {opt['path_checkpoint']} not found.")
     
     # Add parameters for tracking
+    opt['input_dim'] = opt['n_channels'] if opt['target'] in ['channels', 'full'] else opt['sequence_length']
     opt['output_dim'] = opt['channels_out'] if opt['target'] in ['channels', 'full'] else opt['n_channels']
     opt['output_dim_2'] = opt['sequence_length'] if opt['target'] in ['channels'] else opt['timeseries_out']
     
