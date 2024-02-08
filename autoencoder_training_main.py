e544d6d30f2ce25377fb48521658be2b887c37e1
Chad C Williams <ccwillia@uvic.ca>
2024-02-08 08:30:00 -0800

    Revert

--- autoencoder_training_main.py
+++ autoencoder_training_main.py
@@ -120,13 +120,6 @@ def main():
         raise FileNotFoundError(f"Checkpoint file {opt['path_checkpoint']} not found.")
     
     # Add parameters for tracking
-    '''
-    #DEBUG: These are my version:
-    opt['input_dim'] = opt['n_channels'] if opt['target'] in ['channels', 'full'] else opt['sequence_length']
-    opt['output_dim'] = opt['channels_out'] if opt['target'] in ['channels', 'full'] else opt['timeseries_out']
-    opt['output_dim_2'] = opt['sequence_length'] if opt['target'] in ['channels'] else opt['n_channels']
-    '''
-    opt['input_dim'] = opt['n_channels'] if opt['target'] in ['channels', 'full'] else opt['sequence_length']
     opt['output_dim'] = opt['channels_out'] if opt['target'] in ['channels', 'full'] else opt['n_channels']
     opt['output_dim_2'] = opt['sequence_length'] if opt['target'] in ['channels'] else opt['timeseries_out']
     
