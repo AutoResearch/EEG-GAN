b4f53ca055aa593d470049aaf8b9dfad25c1f01a
Chad C Williams <ccwillia@uvic.ca>
2024-02-13 09:14:47 -0800

    Added trained autoencoders

---
+++ trained_ae/rename_autoencoders.py
@@ -0,0 +1,19 @@
+import torch
+
+import os
+from os import listdir
+from os.path import isfile, join
+
+from tqdm import tqdm
+
+
+ae_path = 'trained_ae/Reinforcement Learning'
+if not os.path.isdir(f"{ae_path}/renamed"):
+    os.mkdir(f"{ae_path}/renamed") 
+
+files = [f for f in listdir(ae_path) if isfile(join(ae_path, f))]
+
+for file in tqdm(files):
+    state_dict = torch.load(f"{ae_path}/{file}", map_location=torch.device('cpu'))
+    new_name = state_dict['configuration']['path_dataset'].split('/')[-1].replace('ganTrialElectrodeERP','ae_ep2000').replace('csv','pt')
+    torch.save(state_dict, f"{ae_path}/renamed/{new_name}")
