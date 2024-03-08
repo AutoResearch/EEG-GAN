17506b7eb6fbb5ab6ec4c5ca22118f0a7df33546
Chad C Williams <ccwillia@uvic.ca>
2024-03-07 18:43:02 -0800

    Added classifications to resolve merge conflict

---
+++ classification/gansEEG_Classification.py
@@ -0,0 +1,224 @@
+###############################################
+## LOAD MODULES                              ##
+###############################################
+import numpy as np
+from sklearn.preprocessing import scale
+import random as rnd
+import time
+from tqdm import tqdm
+
+from helpers.dataloader import Dataloader
+from classification_functions import *
+
+###############################################
+## USER INPUTS                               ##
+###############################################
+features = False #Datatype: False = Full Data, True = Features data
+autoencoder = False #Whether to use autoencoder feature selection
+validationOrTest = 'validation' #'validation' or 'test' set to predict
+dataSampleSizes = ['005','010','015','020','030','060','100'] #Which sample sizes to include
+syntheticDataOptions = ['vae'] #The code will iterate through this list. emp = empirical classifications, gan = gan-augmented classifications, vae = vae-augmented classification, over = oversampling classification
+classifiers = ['NN', 'SVM', 'LR'] #The code will iterate through this list
+electrode_number = 1
+
+###############################################
+## SETUP                                     ##
+###############################################
+
+#Base save file names
+augFilename = f'classification/Classification Results/augmentedPredictions_e{electrode_number}_XX.csv'
+empFilename = f'classification/Classification Results/empiricalPredictions_e{electrode_number}_XX.csv'
+ovsFilename = f'classification/Classification Results/oversamplingPredictions_e{electrode_number}_XX.csv'
+vaeFilename = f'classification/Classification Results/vaePredictions_e{electrode_number}_XX.csv'
+
+#Add features tag if applied
+if features:
+    augFilename = augFilename.split('.csv')[0]+'_Features.csv'
+    empFilename = empFilename.split('.csv')[0]+'_Features.csv'
+    ovsFilename = ovsFilename.split('.csv')[0]+'_Features.csv'
+    vaeFilename = vaeFilename.split('.csv')[0]+'_Features.csv'
+
+if autoencoder:
+    augFilename = augFilename.split('.csv')[0]+'_AE.csv'
+    empFilename = empFilename.split('.csv')[0]+'_AE.csv'
+    ovsFilename = ovsFilename.split('.csv')[0]+'_AE.csv'
+    vaeFilename = vaeFilename.split('.csv')[0]+'_AE.csv'
+
+#Add test tag if test set being used
+if validationOrTest == 'test':
+    augFilename = augFilename.split('.csv')[0]+'_TestClassification.csv'
+    empFilename = empFilename.split('.csv')[0]+'_TestClassification.csv'
+    ovsFilename = ovsFilename.split('.csv')[0]+'_TestClassification.csv'
+    vaeFilename = vaeFilename.split('.csv')[0]+'_TestClassification.csv'
+    
+#Display parameters
+print('data Sample Sizes: ' )
+print(dataSampleSizes)
+print('Classification Data: ' + validationOrTest)
+print('GAN-Augmented Filename: ' + augFilename)
+print('VAE-Augmented Filename: ' + vaeFilename)
+print('Empirical Filename: ' + empFilename) 
+print('Oversampling Filename: ' + ovsFilename)
+
+if autoencoder and not features:
+    print('Features: Autoencoder Features')
+elif features and not autoencoder:
+    print('Features: Extracted Features')
+
+###############################################
+## LOAD VALIDATION/TEST DATA                 ##
+###############################################
+
+#Load data 
+y_test, x_test = load_test_data(validationOrTest, electrode_number, features)
+
+###############################################
+## CLASSIFICATION                            ##
+###############################################
+for classifier in classifiers: #Iterate through classifiers (neural network, support vector machine, logistic regression)
+    
+    #Determine current filenames
+    currentAugFilename = augFilename.replace('XX',classifier)
+    currentEmpFilename = empFilename.replace('XX',classifier)
+    currentOvsFilename = ovsFilename.replace('XX',classifier)
+    currentVaeFilename = vaeFilename.replace('XX',classifier)
+
+    loop_analysis = tqdm(syntheticDataOptions)
+    for addSyntheticData in loop_analysis: #Iterate through analyses (empirical, augmented)
+        loop_analysis.set_description(f'Analysis: {"Augmented" if addSyntheticData else "Empirical"}')
+        
+        #Open corresponding file to write to
+        if addSyntheticData=='gan':
+            f = open(currentAugFilename, 'a')
+        elif addSyntheticData=='emp':
+            f = open(currentEmpFilename, 'a')
+        elif addSyntheticData=='over':
+            f = open(currentOvsFilename, 'a')
+        elif addSyntheticData=='vae':
+            f = open(currentVaeFilename, 'a')
+        else:
+            raise NotImplementedError('Analysis index not recognized.')
+
+        loop_data = tqdm(dataSampleSizes)
+        for dataSampleSize in loop_data: #Iterate through sample sizes
+            loop_data.set_description(f'Sample Size: {dataSampleSize}')
+            loop_run = tqdm(range(5))  
+            for run in loop_run: #Conduct analyses 5 times per sample size
+                loop_run.set_description(f'Analysis: {addSyntheticData}, Class: {classifier}, SS: {dataSampleSize}, Run: {run}')
+                
+                ###############################################
+                ## AUTOENCODE TEST DATA                      ##
+                ###############################################
+                if autoencoder:
+                    if addSyntheticData == 'emp': #Empirical
+                        autoencoder_filename = f'trained_ae/Reinforcement Learning/Features Empirical/feat_emp_ae_e{electrode_number}_SS{dataSampleSize}_Run0{run}.pt'
+                    elif addSyntheticData == 'gan': #Augmented
+                        autoencoder_filename = f'trained_ae/Reinforcement Learning/Features Augmented/feat_aug_ae_e{electrode_number}_SS{dataSampleSize}_Run0{run}.pt'
+                    y_test, x_test = load_test_data(validationOrTest, electrode_number, features, autoencoder_filename)
+                else:
+                    autoencoder_filename = None
+
+                ###############################################
+                ## SYNTHETIC PROCESSING                      ##
+                ###############################################
+                if addSyntheticData == 'gan':
+                    synFilename_0 = f"generated_samples/Reinforcement Learning/aegan_ep2000_p500_e{electrode_number}_SS{dataSampleSize}_Run0{run}_c0.csv"
+                    synFilename_1 = f"generated_samples/Reinforcement Learning/aegan_ep2000_p500_e{electrode_number}_SS{dataSampleSize}_Run0{run}_c1.csv"
+                    syn_Y_train, syn_X_train = load_synthetic(synFilename_0, synFilename_1, features, autoencoder_filename)
+
+                elif addSyntheticData == 'vae':
+                    synFilename_0 = f"generated_samples/Reinforcement Learning/vae_e{electrode_number}_SS{dataSampleSize}_Run0{run}_c0.csv"
+                    synFilename_1 = f"generated_samples/Reinforcement Learning/vae_e{electrode_number}_SS{dataSampleSize}_Run0{run}_c1.csv"
+                    syn_Y_train, syn_X_train = load_synthetic(synFilename_0, synFilename_1, features, autoencoder_filename)
+
+                ###############################################
+                ## EMPIRICAL PROCESSING                      ##
+                ###############################################
+                
+                #Load empirical data
+                tempFilename = f'data/Reinforcement Learning/Training Datasets/ganTrialElectrodeERP_p500_e{electrode_number}_SS{dataSampleSize}_Run0{run}.csv'
+                EEGData_metadata = np.genfromtxt(tempFilename, delimiter=',', skip_header=1)[:,:4]
+                EEGData_metadata_3D = EEGData_metadata[EEGData_metadata[:,3] == np.unique(EEGData_metadata[:,3])[0],:]
+                EEGData_dataloader = Dataloader(tempFilename, col_label='Condition', channel_label='Electrode')
+                EEGData = EEGData_dataloader.get_data(shuffle=False).detach().numpy()
+
+                #Oversampling analysis
+                if addSyntheticData == 'over':
+                    num_participant = np.unique(EEGData_metadata[:,0]).shape[0]
+                    participant_IDs = np.unique(EEGData_metadata[:,0])[:50]
+                    participant_cycle = 1
+                    for pi, participant_ID in enumerate(participant_IDs):
+                        participant_index = EEGData_metadata_3D[:,0]==participant_ID
+                        participant_EEGData_meta = EEGData_metadata_3D[participant_index,:]
+                        participant_EEGData_meta[:,0] = participant_EEGData_meta[:,0] + (1000*participant_cycle)
+                        EEGData_metadata_3D = np.vstack([EEGData_metadata_3D, participant_EEGData_meta])
+
+                        participant_EEGData = EEGData[participant_index,:,:]
+                        EEGData = np.vstack([EEGData, participant_EEGData])
+
+                        if pi % num_participant == 0 and pi > 0:
+                            participant_cycle += 1
+
+                #Encode data
+                if autoencoder_filename != None:
+                    EEGData = encode_data(autoencoder_filename, EEGData)
+                
+                #Average data per participant and condition
+                EEGData = averageEEG(EEGData_metadata_3D[:,0], EEGData)
+
+                #Extract outcome and feature data
+                Y_train = EEGData[:,0,0]
+                
+                if features: #If extracting features
+                    X_train = np.array(extractFeatures(EEGData[:,1:,:])) #Extract features
+                    X_train = np.array([train_sample.T.flatten() for train_sample in X_train])
+                    X_train = scale(X_train, axis=0) #Scale data within each trial
+                else:
+                    X_train = np.array([train_sample.T.flatten() for train_sample in EEGData[:,1:,:]])
+                    X_train = scale(X_train, axis=1) #Scale across timeseries within trials
+                
+                #Create augmented dataset
+                if addSyntheticData == 'gan' or addSyntheticData == 'vae': #If this is augmented analyses
+                    Y_train = np.concatenate((Y_train,syn_Y_train)) #Combine empirical and synthetic outcomes
+                    X_train = np.concatenate((X_train,syn_X_train)) #Combine empirical and synthetic features
+                    
+                #Shuffle order of samples
+                trainShuffle = rnd.sample(range(len(X_train)),len(X_train))
+                X_train = X_train[trainShuffle,:]
+                Y_train = Y_train[trainShuffle]
+
+                ###############################################
+                ## CLASSIFIER                                ##
+                ###############################################
+                
+                #Begin timer
+                startTime = time.time()
+                
+                #Run classifier
+                if classifier == 'NN':
+                    optimal_params, predictScore = neuralNetwork(X_train, Y_train, x_test, y_test)
+                elif classifier == 'SVM':
+                    optimal_params, predictScore = supportVectorMachine(X_train, Y_train, x_test, y_test)
+                elif classifier == 'LR':
+                    optimal_params, predictScore = logisticRegression(X_train, Y_train, x_test, y_test)
+                else:
+                    print('Unknown classifier')
+                    optimal_params = []
+                    predictScore = []
+                            
+                ###############################################
+                ## SAVE DATA                                 ##
+                ###############################################
+                    
+                #Create list of what to write
+                toWrite = [str(dataSampleSize),str(run),'0',str(predictScore),str(time.time()-startTime),optimal_params.best_params_]
+
+                #Write data to file
+                for currentWrite in toWrite: #Iterate through write list
+                    f.write(str(currentWrite)) #Write current item
+                    if not currentWrite==toWrite[-1]: #If not the last item
+                        f.write(',') #Add comma
+                f.write('\n') #Creates new line
+                f.flush() #Clears the internal buffer
+
+        f.close() #Close file
---
+++ classification/gansEEG_Classification_Plot.py
@@ -0,0 +1,225 @@
+###############################################
+## IMPORT MODULES                            ##
+###############################################
+import matplotlib.pyplot as plt
+import numpy as np
+
+###############################################
+## SETUP                                     ##
+###############################################
+
+#Determine the sample sizes of interest
+xLabels = [5,10,15,20,30,60,100]
+electrodes = 1
+augmentation_type = 'gan'
+combined = True #Whether to add multiple augmented data to a single plot
+
+data = np.arange(1,10) if electrodes < 8 else np.arange(1,7)
+
+if augmentation_type == 'vae': #Temporary
+    data = np.arange(1,7)
+
+###############################################
+## FUNCTIONS                                 ##
+###############################################
+
+#Define function to determine filenames
+def retrieveData(data, augmentation_type): 
+    if augmentation_type == 'gan':  
+        aug_prefix = 'augmented'
+    elif augmentation_type == 'vae':
+        aug_prefix = 'vae'
+
+    ##Full Time-Series
+    if data == 1:
+        augData = f'classification/Classification Results/{aug_prefix}Predictions_e{electrodes}_NN.csv'
+        empData = f'classification/Classification Results/empiricalPredictions_e{electrodes}_NN.csv'
+    if data == 2:
+        augData = f'classification/Classification Results/{aug_prefix}Predictions_e{electrodes}_SVM.csv'
+        empData = f'classification/Classification Results/empiricalPredictions_e{electrodes}_SVM.csv'
+    if data == 3:
+        augData = f'classification/Classification Results/{aug_prefix}Predictions_e{electrodes}_LR.csv'
+        empData = f'classification/Classification Results/empiricalPredictions_e{electrodes}_LR.csv'
+
+    ##Extracted features
+    if data == 4:
+        augData = f'classification/Classification Results/{aug_prefix}Predictions_e{electrodes}_NN_Features.csv'
+        empData = f'classification/Classification Results/empiricalPredictions_e{electrodes}_NN_Features.csv'
+    if data == 5:
+        augData = f'classification/Classification Results/{aug_prefix}Predictions_e{electrodes}_SVM_Features.csv'
+        empData = f'classification/Classification Results/empiricalPredictions_e{electrodes}_SVM_Features.csv'
+    if data == 6:  
+        augData = f'classification/Classification Results/{aug_prefix}Predictions_e{electrodes}_LR_Features.csv'
+        empData = f'classification/Classification Results/empiricalPredictions_e{electrodes}_LR_Features.csv'
+
+    ##Autoencoder features
+    if data == 7:
+        augData = f'classification/Classification Results/{aug_prefix}Predictions_e{electrodes}_NN_AE.csv'
+        empData = f'classification/Classification Results/empiricalPredictions_e{electrodes}_NN_AE.csv'
+    if data == 8:
+        augData = f'classification/Classification Results/{aug_prefix}Predictions_e{electrodes}_SVM_AE.csv'
+        empData = f'classification/Classification Results/empiricalPredictions_e{electrodes}_SVM_AE.csv'
+    if data == 9:  
+        augData = f'classification/Classification Results/{aug_prefix}Predictions_e{electrodes}_LR_AE.csv'
+        empData = f'classification/Classification Results/empiricalPredictions_e{electrodes}_LR_AE.csv'
+        
+    return empData, augData
+
+
+#Define function to load and plot data
+def loadAndPlot(filename, plotColor, legendName, alpha=1):
+    
+    #Load data
+    data = []
+    with open(filename) as f:
+        [data.append(line.split(',')[0:4]) for line in f.readlines()]
+    data = np.asarray(data).astype(int)
+    
+    #Process descriptive statistics of means and standard errors of the mean
+    meanData = []
+    semData = []
+    for ss in np.unique(data[:,0]):
+        ssIndex = data[:,0] == ss #Sample size
+        meanData.append(np.mean(data[ssIndex,3])) #Mean
+        semData.append((np.std(data[ssIndex,3]))/np.sqrt(len(data[ssIndex,3]))) #Standard error of the mean
+        
+    #Plot Data
+    plt.plot(np.unique(data[:,0]), meanData, color = plotColor, linewidth = 1, alpha=alpha)
+    plt.scatter(np.unique(data[:,0]),meanData,label='_nolegend_', color = plotColor, s = 10, alpha=alpha, linewidths=0)
+    markers, caps, bars = plt.errorbar(np.unique(data[:,0]), meanData, semData, label='_nolegend_', color = plotColor, fmt=' ', linewidth = 1, alpha=alpha)
+    [bar.set_alpha(alpha) for bar in bars]
+    [cap.set_alpha(alpha) for cap in caps]
+        
+    return legendName
+
+#Define function to plot difference bars
+def plotDiffData():
+    
+    #Load empirical data
+    nnDataDS = []
+    with open(empData) as f:
+        [nnDataDS.append(line.split(',')[0:4]) for line in f.readlines()]
+    nnDataDS = np.asarray(nnDataDS).astype(int)
+
+    #Load synthetic data
+    nnDataDSSyn_SynP100 = []
+    with open(augData) as f:
+        [nnDataDSSyn_SynP100.append(line.split(',')[0:4]) for line in f.readlines()]
+    nnDataDSSyn_SynP100 = np.asarray(nnDataDSSyn_SynP100).astype(int)
+
+    #Determine means of empirical data
+    meanDataDS = []
+    for ss in np.unique(nnDataDS[:,0]):
+        ssIndex = nnDataDS[:,0] == ss
+        meanDataDS.append(np.mean(nnDataDS[ssIndex,3]))
+
+    #Determine means of synthetic data
+    meanDataDSSyn_SynP100 = []
+    for ss in np.unique(nnDataDSSyn_SynP100[:,0]):
+        ssIndex = nnDataDSSyn_SynP100[:,0] == ss
+        meanDataDSSyn_SynP100.append(np.mean(nnDataDSSyn_SynP100[ssIndex,3]))
+
+    #Determine mean differences
+    meanDiff = []
+    for ss in range(len(meanDataDSSyn_SynP100)):
+        meanDiff.append(meanDataDSSyn_SynP100[ss]-meanDataDS[ss])
+      
+    #Plot mean differences as bars with annotated labels  
+    ax2.bar(xLabels,meanDiff,color='grey',width=4)
+    for i in range(len(meanDataDSSyn_SynP100)):
+        if meanDiff[i] > 0:
+            ax2.annotate(str(round(meanDiff[i]))+'%', (xLabels[i],meanDiff[i]+.5), ha='center', color='grey', size = 3)
+        else:
+            ax2.annotate(str(round(meanDiff[i]))+'%', (xLabels[i],.5), ha='center', color='grey', size = 3)   
+    
+###############################################
+## SETUP FIGURE                              ##
+###############################################
+
+fig = plt.figure(figsize=(24, 3), dpi=600)
+fig.subplots_adjust(hspace=.3)
+plt.rcParams.update({'font.size': 5})  
+
+ylims = 80 if electrodes == 1 else 85
+alpha = 0.6 if combined else 1
+
+###############################################
+## PLOT NEURAL NETWORK                       ##
+###############################################
+
+#Iterate through all subplots
+for dat in data:
+    
+    #Signify subplot
+    ax1 = plt.subplot(3,3,dat)
+
+    #Load and plot data while extracting legend names
+    legendNames = []
+    empData, augData = retrieveData(dat, augmentation_type)
+    legendNames.append(loadAndPlot(augData,'C0',f'{augmentation_type.upper()}-Augmented',alpha=alpha))
+    legendNames.append(loadAndPlot(empData,'C1','Empirical',alpha=alpha))
+
+    if combined and dat < 7: #DAT < 7 temporary for VAE
+        aug_type = 'vae' if augmentation_type == 'gan' else 'gan'
+        _, augData2 = retrieveData(dat, aug_type)
+        legendNames.append(loadAndPlot(augData2,'C2',f'{aug_type.upper()}-Augmented',alpha=alpha))
+
+    #Create horizontal lines
+    axisLevels = np.arange(50,ylims,5)
+    for y in axisLevels:
+        plt.axhline(y=y, color='k', linestyle=':', alpha=.1)
+        
+    #Formal plot
+    plt.ylim(40,ylims)
+    plt.xlim(0,105)
+    plt.xticks(xLabels)
+    plt.yticks(np.arange(50,ylims,5))
+    plt.xticks(rotation=90)
+    ax1.spines[['right', 'top']].set_visible(False)
+        
+    #Plot legend on last subplot
+    if dat == 3:
+        plt.legend(legendNames, bbox_to_anchor=(.95,1.2), frameon=False)
+        
+    #Plot y label on left subplots
+    if (dat == 1) | (dat == 4) | (dat == 7):
+        plt.ylabel('Prediction Accuracy (%)')
+        
+    #Plot x label on bottom subplots
+    if dat > 6:    
+        plt.xlabel('Sample Size')
+    
+    #Add data type titles
+    if dat == 1:
+        ax1.annotate('Full Time Series',(3,ylims), fontsize = 6)
+    elif dat == 4:
+        ax1.annotate('Extracted Features',(3,ylims), fontsize = 6)
+    elif dat == 7:
+        ax1.annotate('Autoencoder Features',(3,ylims), fontsize = 6)
+        
+    #Add classifier titles
+    if (dat == 1):
+        ax1.annotate('Neural Network', (3,ylims-2.5), fontsize = 5)
+    elif (dat == 2):
+        ax1.annotate('Support Vector Machine', (3,ylims-2.5), fontsize = 5)
+    elif (dat == 3):
+        ax1.annotate('Logistic Regression', (3,ylims-2.5), fontsize = 5)
+
+    #Add difference bars
+    ax2 = ax1.twinx()  
+    plotDiffData()
+    
+    #Format difference bars
+    ax2.set_ylim(0,50)
+    ax2.spines[['right', 'top']].set_visible(False)
+    ax2.set_yticks([])
+    
+###############################################
+## SAVE PLOT                                 ##
+###############################################
+fig = plt.gcf()
+fig.set_size_inches(8, 6)
+if combined:
+    fig.savefig(f'classification/Figures/Figure N - Combined Classification Results (e{electrodes}).png', dpi=600, facecolor='white', edgecolor='none')
+else:
+    fig.savefig(f'classification/Figures/Figure N - {augmentation_type.upper()} Classification Results (e{electrodes}).png', dpi=600, facecolor='white', edgecolor='none')
