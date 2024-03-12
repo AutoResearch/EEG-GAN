89a9708131e0c54c5dadd2f71a6debe5f7e9afe1
Chad C Williams <ccwillia@uvic.ca>
2024-03-11 22:26:41 -0700

    Updated augmented dataset builder

--- data/eeggan_build_augmented_datasets.py
+++ data/eeggan_build_augmented_datasets.py
@@ -1,19 +1,27 @@
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
 
 data_path = f'data/Reinforcement Learning/Training Datasets'
 generated_samples_path = f'generated_samples/Reinforcement Learning'
 augmented_path = f'data/Reinforcement Learning/Augmented Training Datasets'
 
-for electrode in [1, 2]:
-    for sample_size in ['005', '010', '015', '020', '030', '060', '100']:
-        for run in [0, 1, 2, 3, 4]:
+electrodes = [1, 2, 8]
+sample_sizes = ['005', '010', '015', '020', '030', '060', '100']
+runs = [0, 1, 2, 3, 4]
+
+loop = tqdm(total=len(electrodes)*len(sample_sizes)*len(runs))
+for electrode in electrodes:
+    for sample_size in sample_sizes:
+        for run in runs:
+            loop.update(1)
+            loop.set_description(f'Elec: {electrode} ||| SS: {sample_size} ||| Run: {run}')
             empirical_filename = f'{data_path}/ganTrialElectrodeERP_p500_e{electrode}_SS{sample_size}_Run0{run}.csv'
             empirical_data = pd.read_csv(empirical_filename)
 
-            generated_samples_filename_0 = f'{generated_samples_path}/aegan_ep2000_p500_e{electrode}_SS{sample_size}_Run0{run}_c0.csv'
+            generated_samples_filename_0 = f'{generated_samples_path}/vae_e{electrode}_SS{sample_size}_Run0{run}_c0.csv'
             generated_data_0 = pd.read_csv(generated_samples_filename_0)
-            generated_samples_filename_1 = f'{generated_samples_path}/aegan_ep2000_p500_e{electrode}_SS{sample_size}_Run0{run}_c1.csv'
+            generated_samples_filename_1 = f'{generated_samples_path}/vae_e{electrode}_SS{sample_size}_Run0{run}_c1.csv'
             generated_data_1 = pd.read_csv(generated_samples_filename_1)
             generated_data = pd.concat([generated_data_0, generated_data_1])
 
@@ -23,6 +31,7 @@ for electrode in [1, 2]:
                 generated_data = generated_data.rename(columns={f'Time{timepoint}': f'Time{timepoint+1}'})
 
             augmented_data = pd.concat([empirical_data, generated_data])
-            augmented_data.to_csv(f'{augmented_path}/augmentedData_p500_e{electrode}_SS{sample_size}_Run0{run}.csv')
+            augmented_data.to_csv(f'{augmented_path}/vae_augmentedData_e{electrode}_SS{sample_size}_Run0{run}.csv')
+loop.close()
 
 
