bd26422684fec1ee50cbb77fb33b81ab267d13d2
Chad C Williams <cwilliams.uca@gmail.com>
2024-04-18 14:47:00 -0700

    debug

--- helpers/ddp_training.py
+++ helpers/ddp_training.py
@@ -130,11 +130,9 @@ class AEDDPTrainer(trainer.AETrainer):
 def run(rank, world_size, master_port, backend, trainer_ddp, opt):
     try:
         _setup(rank, world_size, master_port, backend)
-        print('1')
         trainer_ddp = _setup_trainer(rank, trainer_ddp)
         _ddp_training(trainer_ddp, opt)
-        print('2')
-        #dist.destroy_process_group()
+        dist.destroy_process_group()
     except Exception as error:
         ValueError(f"Error in DDP training: {error}")
         dist.destroy_process_group()
@@ -166,35 +164,38 @@ def _setup_trainer(rank, trainer_ddp):
 
 def _ddp_training(trainer_ddp, opt):
     # load data
-    if 'conditions' not in opt:
-        opt['conditions'] = ['']
-    dataloader = Dataloader(opt['data'],
-                            kw_time=opt['kw_timestep'],
-                            kw_conditions=opt['conditions'],
-                            norm_data=True,
-                            kw_channel=opt['kw_channel'])
-    dataset = dataloader.get_data()
-    opt['sequence_length'] = dataset.shape[2] - dataloader.labels.shape[2]
+    try:
+        if 'conditions' not in opt:
+            opt['conditions'] = ['']
+        dataloader = Dataloader(opt['data'],
+                                kw_time=opt['kw_timestep'],
+                                kw_conditions=opt['conditions'],
+                                norm_data=True,
+                                kw_channel=opt['kw_channel'])
+        dataset = dataloader.get_data()
+        opt['sequence_length'] = dataset.shape[2] - dataloader.labels.shape[2]
 
-    if trainer_ddp.batch_size > len(dataset):
-        raise ValueError(f"Batch size {trainer_ddp.batch_size} is larger than the partition size {len(dataset)}.")
+        if trainer_ddp.batch_size > len(dataset):
+            raise ValueError(f"Batch size {trainer_ddp.batch_size} is larger than the partition size {len(dataset)}.")
 
-    # train
-    if isinstance(trainer_ddp, GANDDPTrainer):
-        path = 'trained_models'
-        model_prefix = 'gan'
-        dataset = DataLoader(dataset, batch_size=trainer_ddp.batch_size, shuffle=True)
-        gen_samples = trainer_ddp.training(dataset)
-    elif isinstance(trainer_ddp, AEDDPTrainer):
-        path = 'trained_ae'
-        model_prefix = 'ae'
-        train_data = dataset[:int(len(dataset) * opt['train_ratio'])]
-        test_data = dataset[int(len(dataset) * opt['train_ratio']):]
-        train_data = DataLoader(train_data, batch_size=trainer_ddp.batch_size, shuffle=True)
-        test_data = DataLoader(test_data, batch_size=trainer_ddp.batch_size, shuffle=True)
-        trainer_ddp.training(train_data, test_data)
-    else:
-        raise ValueError(f"Trainer type {type(trainer_ddp)} not supported.")
+        # train
+        if isinstance(trainer_ddp, GANDDPTrainer):
+            path = 'trained_models'
+            model_prefix = 'gan'
+            dataset = DataLoader(dataset, batch_size=trainer_ddp.batch_size, shuffle=True)
+            gen_samples = trainer_ddp.training(dataset)
+        elif isinstance(trainer_ddp, AEDDPTrainer):
+            path = 'trained_ae'
+            model_prefix = 'ae'
+            train_data = dataset[:int(len(dataset) * opt['train_ratio'])]
+            test_data = dataset[int(len(dataset) * opt['train_ratio']):]
+            train_data = DataLoader(train_data, batch_size=trainer_ddp.batch_size, shuffle=True)
+            test_data = DataLoader(test_data, batch_size=trainer_ddp.batch_size, shuffle=True)
+            trainer_ddp.training(train_data, test_data)
+        else:
+            raise ValueError(f"Trainer type {type(trainer_ddp)} not supported.")
+    except Exception as error:
+        ValueError(f"Error in DDP training: {error}")
 
     # save checkpoint
     if trainer_ddp.rank == 0:
