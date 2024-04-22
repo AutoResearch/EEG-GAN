903665217978f03ec7a372638eb778ffde3b83e0
Chad C Williams <cwilliams.uca@gmail.com>
2024-04-22 07:57:16 -0700

    Removed debug print

--- helpers/trainer.py
+++ helpers/trainer.py
@@ -159,8 +159,6 @@ class GANTrainer(Trainer):
         gen_samples_batch = None
         batch = None
 
-        print(self.epochs)
-
         loop = tqdm(range(self.epochs))
         # try/except for KeyboardInterrupt --> Abort training and save model
         try:
