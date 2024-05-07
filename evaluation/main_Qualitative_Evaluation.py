79e2717fae9fe0f527e6a723950bc8ca36cf24a7
Chad C Williams <cwilliams.uca@gmail.com>
2024-05-07 12:43:29 -0700

    Finished main plotting

--- evaluation/main_Qualitative_Evaluation.py
+++ evaluation/main_Qualitative_Evaluation.py
@@ -107,12 +107,14 @@ def time_frequency_transform(data, speriod=1/1000, label=''):
 ###############################################
 ## LOAD AND PROCESS DATA                     ##
 ###############################################
-def main(run_TFT=True, process_synthetic=True, run_gan=True, run_vae=True, component='N400'):
-
-    print('Loading data...')
+def main():
     
     ## EMPIRICAL ##
     def load_data(data, gan_data, vae_data, run_gan=True, run_vae=True, process_synthetic=True):
+        
+        print('Loading data...')
+        print(data)
+
         #Load EEG Data (Participant, Condition, Trial, Electrode, Time1, ...)
         EEG_data = np.genfromtxt(data, delimiter=',', skip_header=1)[:,1:] #Removes Participant Column
         EEG_data = np.delete(EEG_data, 1, 1) #Delete Unused Column (Trial)
@@ -120,8 +122,6 @@ def main(run_TFT=True, process_synthetic=True, run_gan=True, run_vae=True, compo
         #Split into conditions
         c1_EEG_data = EEG_data[np.r_[EEG_data[:,0]==1],2:]  
         c0_EEG_data = EEG_data[np.r_[EEG_data[:,0]==0],2:] 
-        c1_EEG_data = np.mean(c1_EEG_data, axis=0)
-        c0_EEG_data = np.mean(c0_EEG_data, axis=0)
 
         if run_gan:
             ## GAN ##
@@ -143,29 +143,29 @@ def main(run_TFT=True, process_synthetic=True, run_gan=True, run_vae=True, compo
                 tempganData = ganData[:,1:]
             
             #Create and populate new array for processed synthetic data
-            processedganData = np.zeros((len(tempganData),129))
+            processedganData = np.zeros((len(tempganData),tempganData[0].shape[0]+1))
             processedganData[:,0] = ganData[:,0]
             processedganData[:,1:] = np.array(tempganData)
 
             #Split into conditions
-            lossganData = processedganData[np.r_[processedganData[:,0]==1],1:]
-            winganData = processedganData[np.r_[processedganData[:,0]==0],1:]
+            c1ganData = processedganData[np.r_[processedganData[:,0]==1],1:]
+            c0ganData = processedganData[np.r_[processedganData[:,0]==0],1:]
 
             #Average data
-            avgLossganData = np.mean(lossganData, axis=0)
-            avgWinganData = np.mean(winganData, axis=0)
+            avgc1ganData = np.mean(c1ganData, axis=0)
+            avgc0ganData = np.mean(c0ganData, axis=0)
 
             #Scale synthetic data 
-            EEGDataScale = np.max(c1_EEG_data)-np.min(c1_EEG_data) 
-            EEGDataOffset = np.min(c1_EEG_data)
-            ganDataScale = np.max(avgLossganData)-np.min(avgLossganData)
-            ganDataOffset = np.min(avgLossganData)
+            EEGDataScale = np.max(np.mean(c1_EEG_data,axis=0))-np.min(np.mean(c1_EEG_data,axis=0)) 
+            EEGDataOffset = np.min(np.mean(c1_EEG_data,axis=0))
+            ganDataScale = np.max(avgc1ganData)-np.min(avgc1ganData)
+            ganDataOffset = np.min(avgc1ganData)
 
-            avgLossganData = (((avgLossganData-ganDataOffset)/ganDataScale)*EEGDataScale)+EEGDataOffset
-            avgWinganData = (((avgWinganData-ganDataOffset)/ganDataScale)*EEGDataScale)+EEGDataOffset
+            avgc1ganData = (((avgc1ganData-ganDataOffset)/ganDataScale)*EEGDataScale)+EEGDataOffset
+            avgc0ganData = (((avgc0ganData-ganDataOffset)/ganDataScale)*EEGDataScale)+EEGDataOffset
 
-            scaledLossganData = (((lossganData-ganDataOffset)/ganDataScale)*EEGDataScale)+EEGDataOffset
-            scaledWinganData = (((winganData-ganDataOffset)/ganDataScale)*EEGDataScale)+EEGDataOffset
+            scaledc1ganData = (((c1ganData-ganDataOffset)/ganDataScale)*EEGDataScale)+EEGDataOffset
+            scaledc0ganData = (((c0ganData-ganDataOffset)/ganDataScale)*EEGDataScale)+EEGDataOffset
 
         if run_vae:
             ## VAE ##
@@ -185,41 +185,41 @@ def main(run_TFT=True, process_synthetic=True, run_gan=True, run_vae=True, compo
                 tempvaeData = vaeData[:,1:]
 
             #Create and populate new array for processed synthetic data
-            processedvaeData = np.zeros((len(tempvaeData),129))
+            processedvaeData = np.zeros((len(tempvaeData),tempganData[0].shape[0]+1))
             processedvaeData[:,0] = vaeData[:,0]
             processedvaeData[:,1:] = np.array(tempvaeData)
 
             #Split into conditions
-            lossvaeData = processedvaeData[np.r_[processedvaeData[:,0]==1],1:]
-            winvaeData = processedvaeData[np.r_[processedvaeData[:,0]==0],1:]
+            c1vaeData = processedvaeData[np.r_[processedvaeData[:,0]==1],1:]
+            c0vaeData = processedvaeData[np.r_[processedvaeData[:,0]==0],1:]
 
             #Average data
-            avgLossvaeData = np.mean(lossvaeData, axis=0)
-            avgWinvaeData = np.mean(winvaeData, axis=0)
+            avgc1vaeData = np.mean(c1vaeData, axis=0)
+            avgc0vaeData = np.mean(c0vaeData, axis=0)
 
             #Scale synthetic data 
-            EEGDataScale = np.max(c1_EEG_data)-np.min(c1_EEG_data)
-            EEGDataOffset = np.min(c1_EEG_data)
-            vaeDataScale = np.max(avgLossvaeData)-np.min(avgLossvaeData)
-            vaeDataOffset = np.min(avgLossvaeData)
+            EEGDataScale = np.max(np.mean(c1_EEG_data,axis=0))-np.min(np.mean(c1_EEG_data,axis=0))
+            EEGDataOffset = np.min(np.mean(c1_EEG_data,axis=0))
+            vaeDataScale = np.max(avgc1vaeData)-np.min(avgc1vaeData)
+            vaeDataOffset = np.min(avgc1vaeData)
 
-            avgLossvaeData = (((avgLossvaeData-vaeDataOffset)/vaeDataScale)*EEGDataScale)+EEGDataOffset
-            avgWinvaeData = (((avgWinvaeData-vaeDataOffset)/vaeDataScale)*EEGDataScale)+EEGDataOffset
+            avgc1vaeData = (((avgc1vaeData-vaeDataOffset)/vaeDataScale)*EEGDataScale)+EEGDataOffset
+            avgc0vaeData = (((avgc0vaeData-vaeDataOffset)/vaeDataScale)*EEGDataScale)+EEGDataOffset
 
-            scaledLossvaeData = (((lossvaeData-vaeDataOffset)/vaeDataScale)*EEGDataScale)+EEGDataOffset
-            scaledWinvaeData = (((winvaeData-vaeDataOffset)/vaeDataScale)*EEGDataScale)+EEGDataOffset
+            scaledc1vaeData = (((c1vaeData-vaeDataOffset)/vaeDataScale)*EEGDataScale)+EEGDataOffset
+            scaledc0vaeData = (((c0vaeData-vaeDataOffset)/vaeDataScale)*EEGDataScale)+EEGDataOffset
 
         print('Data loading complete!')
     
-        return c1_EEG_data, c0_EEG_data, scaledWinganData, scaledLossganData, scaledWinvaeData, scaledLossvaeData
+        return c1_EEG_data, c0_EEG_data, scaledc1ganData, scaledc0ganData, scaledc1vaeData, scaledc0vaeData
     
-    REWP_eeg_c0, REWP_eeg_c1, REWP_gan_c0, REWP_gan_c1, REWP_vae_c0, REWP_vae_c1 = load_data(f'data/ERPCORE/REWP/Full Datasets/erpcore_REWP_full_cleaned.csv', 
-                                                                                             f'generated_samples/ERPCORE/REWP/Full Datasets/gan_erpcore_REWP_full_cleaned.csv',
-                                                                                             f'generated_samples/ERPCORE/REWP/Full Datasets/vae_erpcore_REWP_full.csv')
+    REWP_eeg_c0, REWP_eeg_c1, REWP_gan_c0, REWP_gan_c1, REWP_vae_c0, REWP_vae_c1 = load_data(f'data/Reinforcement Learning/Full Datasets/ganTrialElectrodeERP_p500_e1_len100.csv', 
+                                                                                             f'generated_samples/Reinforcement Learning/Full Datasets/gan_ep2000_p500_e1_full.csv',
+                                                                                             f'generated_samples/Reinforcement Learning/Full Datasets/vae_p500_e1_full.csv')
     
-    N2P3_eeg_c0, N2P3_eeg_c1, N2P3_gan_c0, N2P3_gan_c1, N2P3_vae_c0, N2P3_vae_c1 = load_data(f'data/ERPCORE/N2P3/Full Datasets/erpcore_N2P3_full_cleaned.csv', 
-                                                                                             f'generated_samples/ERPCORE/N2P3/Full Datasets/gan_erpcore_N2P3_full_cleaned.csv',
-                                                                                             f'generated_samples/ERPCORE/N2P3/Full Datasets/vae_erpcore_N2P3_full.csv')
+    N2P3_eeg_c0, N2P3_eeg_c1, N2P3_gan_c0, N2P3_gan_c1, N2P3_vae_c0, N2P3_vae_c1 = load_data(f'data/Antisaccade/Full Datasets/antisaccade_left_full.csv', 
+                                                                                             f'generated_samples/Antisaccade/Full Datasets/gan_antisaccade_full.csv',
+                                                                                             f'generated_samples/Antisaccade/Full Datasets/vae_antisaccade_full.csv')
 
     N170_eeg_c0, N170_eeg_c1, N170_gan_c0, N170_gan_c1, N170_vae_c0, N170_vae_c1 = load_data(f'data/ERPCORE/N170/Full Datasets/erpcore_N170_full_cleaned.csv', 
                                                                                              f'generated_samples/ERPCORE/N170/Full Datasets/gan_erpcore_N170_full_cleaned.csv',
@@ -241,32 +241,70 @@ def main(run_TFT=True, process_synthetic=True, run_gan=True, run_vae=True, compo
     ## ERPS
     #######################################
 
-    def plot_ERP(c0, c1, num_item, time, title, num_rows=4):
-        ax1 = plt.subplot(num_rows,3, num_item)
-        plt.plot(time, c0, axis=0)
-        plt.plot(time, c1, axis=0)
-        plt.ylabel(r'Voltage ($\mu$V)')
-        plt.xlabel('Time (ms)')
-        plt.title(title, loc = 'left', x = .02, y=.9)
-        #plt.ylim((-2,14))
+    def plot_ERP(c0, c1, num_item, ylim, num_rows=4):
+
+        #Setup
+        ax1 = plt.subplot(num_rows, 3, num_item)
+        time = np.linspace(0,c0.shape[1],c0.shape[1])
+
+        #Plot
+        plt.plot(time, np.mean(c0,axis=0))
+        plt.plot(time, np.mean(c1,axis=0))
+
+        #Title
+        if num_item == 1:
+            plt.text(0.5, 1.2, 'Empirical', horizontalalignment='center', verticalalignment='center', transform=ax1.transAxes, fontsize=16, fontweight='bold')
+            #plt.title('Empirical', fontsize=16, fontweight='bold')
+        elif num_item == 2:
+            plt.text(0.5, 1.2, 'GAN-Synthetic', horizontalalignment='center', verticalalignment='center', transform=ax1.transAxes, fontsize=16, fontweight='bold')
+            #plt.title('GAN-Synthetic')
+        elif num_item == 3:
+            plt.text(0.5, 1.2, 'VAE-Synthetic', horizontalalignment='center', verticalalignment='center', transform=ax1.transAxes, fontsize=16, fontweight='bold')
+            #plt.title('VAE-Synthetic')
+
+        #Row labels
+        if num_item == 1:
+            plt.text(-0.2, 1.075, 'Reinforcement\nLearning', horizontalalignment='left', verticalalignment='center', transform=ax1.transAxes, fontsize=12, fontweight='bold')
+        elif num_item == 4:
+            plt.text(-0.2, 1.05, 'Anti-Saccade', horizontalalignment='left', verticalalignment='center', transform=ax1.transAxes, fontsize=12, fontweight='bold')
+        elif num_item == 7:
+            plt.text(-0.2, 1.05, 'Face Perception', horizontalalignment='left', verticalalignment='center', transform=ax1.transAxes, fontsize=12, fontweight='bold')
+        elif num_item == 10:
+            plt.text(-0.2, 1.05, 'Visual Search', horizontalalignment='left', verticalalignment='center', transform=ax1.transAxes, fontsize=12, fontweight='bold')
+        
+        #Labels
+        if num_item > 9:
+            plt.xlabel('Time (ms)')
+        if num_item == 1 or num_item == 4 or num_item == 7 or num_item == 10:
+            plt.ylabel( r'Voltage ($\mu$V)')
+
+        #Legend
+        if num_item == 3:
+            plt.legend(['Win', 'Lose'], loc='upper right', fontsize=12, frameon=False, bbox_to_anchor=(1.1, 1.2))
+        elif num_item == 6:
+            plt.legend(['Anti-Saccade', 'Pro-Saccade'], loc='upper right', fontsize=12, frameon=False, bbox_to_anchor=(1.1, 1.2))
+        elif num_item == 9:
+            plt.legend(['Face', 'Car'], loc='upper right', fontsize=12, frameon=False, bbox_to_anchor=(1.1, 1.2))
+        elif num_item == 12:
+            plt.legend(['Ipsilateral', 'Contralateral'], loc='upper right', fontsize=12, frameon=False, bbox_to_anchor=(1.1, 1.2))
+           
+        #Format
+        plt.ylim(ylim)
         ax1.spines.right.set_visible(False)
         ax1.spines.top.set_visible(False)
 
-    plot_ERP(REWP_eeg_c0, REWP_eeg_c1, 1, np.linspace(0,100,100)/100, 'Empirical')
-    plot_ERP(REWP_gan_c0, REWP_gan_c1, 2, np.linspace(0,100,100)/100, 'GAN-Synthetic')
-    plot_ERP(REWP_vae_c0, REWP_vae_c1, 3, np.linspace(0,100,100)/100, 'VAE-Synthetic')
-
-    plot_ERP(N2P3_eeg_c0, N2P3_eeg_c1, 4, np.linspace(0,100,125)/100, '')
-    plot_ERP(N2P3_gan_c0, N2P3_gan_c1, 5, np.linspace(0,100,125)/100, '')
-    plot_ERP(N2P3_vae_c0, N2P3_vae_c1, 6, np.linspace(0,100,125)/100, '')
-
-    plot_ERP(N170_eeg_c0, N170_eeg_c1, 7, np.linspace(0,100,128)/100, '')
-    plot_ERP(N170_gan_c0, N170_gan_c1, 8, np.linspace(0,100,128)/100, '')
-    plot_ERP(N170_vae_c0, N170_vae_c1, 9, np.linspace(0,100,128)/100, '')
-
-    plot_ERP(N2PC_eeg_c0, N2PC_eeg_c1, 10, np.linspace(0,100,128)/100, '')
-    plot_ERP(N2PC_gan_c0, N2PC_gan_c1, 11, np.linspace(0,100,128)/100, '')
-    plot_ERP(N2PC_vae_c0, N2PC_vae_c1, 12, np.linspace(0,100,128)/100, '')
+    plot_ERP(REWP_eeg_c0, REWP_eeg_c1, 1, [-2, 14])
+    plot_ERP(REWP_gan_c0, REWP_gan_c1, 2, [-2, 14])
+    plot_ERP(REWP_vae_c0, REWP_vae_c1, 3, [-2, 14])
+    plot_ERP(N2P3_eeg_c0, N2P3_eeg_c1, 4, [-3, 2])
+    plot_ERP(N2P3_gan_c0, N2P3_gan_c1, 5, [-3, 2])
+    plot_ERP(N2P3_vae_c0, N2P3_vae_c1, 6, [-3, 2])
+    plot_ERP(N170_eeg_c0, N170_eeg_c1, 7, [-3, 10])
+    plot_ERP(N170_gan_c0, N170_gan_c1, 8, [-3, 10])
+    plot_ERP(N170_vae_c0, N170_vae_c1, 9, [-3, 10])
+    plot_ERP(N2PC_eeg_c0, N2PC_eeg_c1, 10, [-3, 8])
+    plot_ERP(N2PC_gan_c0, N2PC_gan_c1, 11, [-3, 8])
+    plot_ERP(N2PC_vae_c0, N2PC_vae_c1, 12, [-3, 8])
 
     #######################################
     ## Save
@@ -278,17 +316,7 @@ def main(run_TFT=True, process_synthetic=True, run_gan=True, run_vae=True, compo
     fig = plt.gcf()
     fig.set_size_inches(12, num_rows*4)
 
-    fig.savefig(f'figures/Figure N - ERPCORE_{component}_Evaluations.png', dpi=600)
+    fig.savefig(f'figures/Figure N - gan_Evaluations.png', dpi=600)
 
 if __name__ == '__main__':
-
-    #User inputs
-    run_TFT = False
-    process_synthetic=True
-
-    run_gan = True
-    run_vae = False
-
-    component = 'N2PC'
-    
-    main(run_TFT=run_TFT, process_synthetic=process_synthetic, run_gan=run_gan, run_vae=run_vae, component=component)
+    main()
