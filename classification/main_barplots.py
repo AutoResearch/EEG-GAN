1a94739845c38a4ba6951fe05b4b89cca2cec9ec
Chad C Williams <cwilliams.uca@gmail.com>
2024-05-23 12:56:47 -0700

    Added main barplots

---
+++ classification/main_barplots.py
@@ -0,0 +1,6 @@
+from main_plot_functions import *
+
+component='N170'
+filename_prefix= 'classification/ERPCORE/N170/'
+save_name = 'figures/Figure SN - N170 Classification Barplot.png'
+plot_main(component=component, targets=[], filename_prefix=filename_prefix, interactive=False, save_name=save_name)
