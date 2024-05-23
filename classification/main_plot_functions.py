527612a205d2df6818e62942b02f7c1278b6b559
Chad C Williams <cwilliams.uca@gmail.com>
2024-05-23 16:54:15 -0700

    Updated interactive barplot

--- classification/main_plot_functions.py
+++ classification/main_plot_functions.py
@@ -16,7 +16,10 @@ class InteractivePlot:
 
         #Setup initial variables
         self.targets = []
+        self.target_labels = []
         self.available_targets, _ = retrieveData(0, 1)
+        self.base_target = None
+        self.ref_target = None
 
         #Setup dataset information
         self.datasets = ['Reinforcement Learning (1E)', 
@@ -37,15 +40,20 @@ class InteractivePlot:
         self.electrode = 1
 
         #Setup widgets
-        self.select_dataset_dropdown = wd.Dropdown(options=self.datasets, value = self.datasets[0], description = 'Select Dataset', disabled = False)
+        style = {'description_width': '100px'}
+        self.select_dataset_dropdown = wd.Dropdown(layout={'width': 'max-content'}, options=self.datasets, value = self.datasets[0], description = ' ', disabled = False, style=style)
         self.targets_checkboxes = [wd.Checkbox(value=False, description=avail_target) for avail_target in self.available_targets]
         self.select_target_row = wd.VBox([wd.HBox(self.targets_checkboxes[:4]), wd.HBox(self.targets_checkboxes[4:])])
-        self.centered_toggle = wd.ToggleButton(value=True, description='Centered')
+        self.centered_toggle = wd.Checkbox(value=False, description='Centered')
+        self.selected_base_dropdown = wd.Dropdown(options=[' '] + self.target_labels, value = ' ', description = ' ', disabled = False, style=style)
+        self.selected_reference_dropdown = wd.Dropdown(options=[' '] + self.target_labels, value = ' ', description = ' ', disabled = False, style=style)
 
         for i in range(len(self.targets_checkboxes)):
             self.targets_checkboxes[i].observe(self.change_targets) 
         self.select_dataset_dropdown.observe(self.change_dataset)
         self.centered_toggle.observe(self.change_and_refresh)
+        self.selected_base_dropdown.observe(self.select_comp)
+        self.selected_reference_dropdown.observe(self.select_comp)
 
         #Display plot
         self.refresh()
@@ -53,17 +61,61 @@ class InteractivePlot:
     def refresh(self):
         clear_output()
 
-        display(self.select_dataset_dropdown) #Display the widget for use
-        display(self.centered_toggle) #Display the widget for use
-        display(self.select_target_row)
+        #Update target labels
+        base_val = self.selected_base_dropdown.value
+        ref_val = self.selected_reference_dropdown.value
+        self.selected_base_dropdown.options=[' '] + self.target_labels
+        self.selected_reference_dropdown.options=[' '] + self.target_labels
+        self.selected_base_dropdown.value = base_val
+        self.selected_reference_dropdown.value = ref_val
 
+        #Setup controls
+        header_style = {'description_width': '150px'}
+        header1 = wd.HTML(description="𝐒𝐞𝐥𝐞𝐜𝐭 𝐃𝐚𝐭𝐚𝐬𝐞𝐭", value="", style=header_style)
+        header2 = wd.HTML(description="𝐒𝐞𝐥𝐞𝐜𝐭 𝐀𝐧𝐚𝐥𝐲𝐬𝐞𝐬", value="", style=header_style)
+        header3 = wd.HTML(description="𝐒𝐞𝐥𝐞𝐜𝐭 𝐂𝐨𝐦𝐩𝐚𝐫𝐢𝐬𝐨𝐧𝐬", value="", style=header_style)
+        header4 = wd.HTML(description="𝐒𝐞𝐥𝐞𝐜𝐭 𝐅𝐨𝐫𝐦𝐚𝐭𝐭𝐢𝐧𝐠", value="", style=header_style)
+        controls = wd.GridBox(children=[header1, self.select_dataset_dropdown, header2, self.select_target_row, header3, self.selected_base_dropdown, self.selected_reference_dropdown, header4, self.centered_toggle],
+        layout=wd.Layout(
+            grid_template_rows='auto auto auto auto auto auto auto auto auto',
+            grid_template_areas='''
+            "header1"
+            "drop1"
+            "header2"
+            "box1"
+            "header3"
+            "drop2"
+            "drop3"
+            "header4"
+            "toggle"
+            ''')
+       )
+        display(controls)
+
+        #Update plot
         self.target_labels = [self.available_targets[i] for i in self.targets]
-        plot_main(targets=self.target_labels, center_toggle=self.centered_toggle.value, filename_prefix=self.filename_prefix, x_max=self.x_max, electrodes=self.electrode)
+        plot_main(targets=self.target_labels, 
+                  base_target=self.selected_base_dropdown.value, 
+                  ref_target=self.selected_reference_dropdown.value, 
+                  center_toggle=not self.centered_toggle.value, 
+                  filename_prefix=self.filename_prefix, 
+                  x_max=self.x_max, 
+                  electrodes=self.electrode)
 
     def change_and_refresh(self, change):
         if change['type'] == 'change' and change['name'] == 'value':
-            #for target_checkbox in self.targets_checkboxes:
-            #    self.targets_checkboxes[i].value = self.centered_toggle.value
+            self.refresh()
+    
+    def select_comp(self, change):
+        if change['type'] == 'change' and change['name'] == 'value':
+            if self.selected_base_dropdown.value != ' ':
+                self.base_target = self.selected_base_dropdown.value
+            else:
+                self.base_target = None
+            if self.selected_reference_dropdown.value != ' ':
+                self.ref_target = self.selected_reference_dropdown.value
+            else:
+                self.ref_target = None
             self.refresh()
 
     def change_dataset(self, change):
@@ -81,6 +133,8 @@ class InteractivePlot:
             for i, target_checkbox in enumerate(self.targets_checkboxes):
                 if target_checkbox.value:
                     self.targets.append(i)
+            self.target_labels = [self.available_targets[i] for i in self.targets]
+            
             self.refresh()
 
 ###############################################
@@ -149,61 +203,66 @@ def loadAndPlot(filename, plotColor, legendName, selected=False, alpha=1, select
 #Define function to plot difference bars
 def plotDiffData(target, reference, analysis_names, filenames):
 
-    meanDataDSSyn_SynP100 = []
-    target_index = [i for i,f in enumerate(analysis_names) if target == f][0]
-    target_filename = filenames[target_index]
-
-    #Load empirical data
-    nnDataDS = []
-    with open(target_filename) as f:
-        [nnDataDS.append(line.split(',')[0:4]) for line in f.readlines()]
-    nnDataDS = np.asarray(nnDataDS).astype(int)
-
-    #Determine means of empirical data
-    meanDataDS = []
-    for ss in np.unique(nnDataDS[:,0]):
-        ssIndex = nnDataDS[:,0] == ss
-        meanDataDS.append(np.mean(nnDataDS[ssIndex,3]))
-
-    if reference:
-        reference_index = [i for i,f in enumerate(analysis_names) if reference == f][0]
-        reference_filename = filenames[reference_index]
-        #Load synthetic data
-        nnDataDSSyn_SynP100 = []
-        with open(reference_filename) as f:
-            [nnDataDSSyn_SynP100.append(line.split(',')[0:4]) for line in f.readlines()]
-        nnDataDSSyn_SynP100 = np.asarray(nnDataDSSyn_SynP100).astype(int)
-
-        #Determine means of synthetic data
-        meanDataDSSyn_SynP100 = []
-        for ss in np.unique(nnDataDSSyn_SynP100[:,0]):
-            ssIndex = nnDataDSSyn_SynP100[:,0] == ss
-            meanDataDSSyn_SynP100.append(np.mean(nnDataDSSyn_SynP100[ssIndex,3]))
-
-        #Determine mean differences
-        meanDiff = []
-        for ss in range(len(meanDataDSSyn_SynP100)):
-            meanDiff.append(meanDataDSSyn_SynP100[ss]-meanDataDS[ss])
-
-    #Plot mean differences as bars with annotated labels 
-    x_range = np.arange(1,5) if len(meanDataDS) == 4 else np.arange(1,8)
-    if reference:
-        plt.bar(x_range,meanDiff,color='grey',width=.5)
-    else:
-        plt.bar(x_range,meanDataDS,color='grey',width=.5)
+    if target == ' ' and reference != ' ':
+        target = reference
+        reference = ' '
     
-    for i in range(len(meanDataDS)):
-        annotation_font_size=8
-        if reference:
-            if meanDiff[i] > 0:
-                plt.annotate(str(round(meanDiff[i]))+'%', (x_range[i],meanDiff[i]+.2), ha='center', color='grey', size = annotation_font_size)
-            else:
-                plt.annotate(str(round(meanDiff[i]))+'%', (x_range[i],.2), ha='center', color='grey', size = annotation_font_size) 
+    if target != ' ':
+        meanDataDSSyn_SynP100 = []
+        target_index = [i for i,f in enumerate(analysis_names) if target == f][0]
+        target_filename = filenames[target_index]
+
+        #Load empirical data
+        nnDataDS = []
+        with open(target_filename) as f:
+            [nnDataDS.append(line.split(',')[0:4]) for line in f.readlines()]
+        nnDataDS = np.asarray(nnDataDS).astype(int)
+
+        #Determine means of empirical data
+        meanDataDS = []
+        for ss in np.unique(nnDataDS[:,0]):
+            ssIndex = nnDataDS[:,0] == ss
+            meanDataDS.append(np.mean(nnDataDS[ssIndex,3]))
+
+        if reference != ' ':
+            reference_index = [i for i,f in enumerate(analysis_names) if reference == f][0]
+            reference_filename = filenames[reference_index]
+            #Load synthetic data
+            nnDataDSSyn_SynP100 = []
+            with open(reference_filename) as f:
+                [nnDataDSSyn_SynP100.append(line.split(',')[0:4]) for line in f.readlines()]
+            nnDataDSSyn_SynP100 = np.asarray(nnDataDSSyn_SynP100).astype(int)
+
+            #Determine means of synthetic data
+            meanDataDSSyn_SynP100 = []
+            for ss in np.unique(nnDataDSSyn_SynP100[:,0]):
+                ssIndex = nnDataDSSyn_SynP100[:,0] == ss
+                meanDataDSSyn_SynP100.append(np.mean(nnDataDSSyn_SynP100[ssIndex,3]))
+
+            #Determine mean differences
+            meanDiff = []
+            for ss in range(len(meanDataDSSyn_SynP100)):
+                meanDiff.append(meanDataDSSyn_SynP100[ss]-meanDataDS[ss])
+
+        #Plot mean differences as bars with annotated labels 
+        x_range = np.arange(1,5) if len(meanDataDS) == 4 else np.arange(1,8)
+        if reference != ' ':
+            plt.bar(x_range,meanDiff,color='grey',width=.5)
         else:
-            plt.annotate(str(round(meanDataDS[i]))+'%', (x_range[i],meanDataDS[i]+.5), ha='center', color='grey', size = annotation_font_size)
+            plt.bar(x_range,meanDataDS,color='grey',width=.5)
         
+        for i in range(len(meanDataDS)):
+            annotation_font_size=8
+            if reference != ' ':
+                if meanDiff[i] > 0:
+                    plt.annotate(str(round(meanDiff[i]))+'%', (x_range[i],meanDiff[i]+.2), ha='center', color='grey', size = annotation_font_size)
+                else:
+                    plt.annotate(str(round(meanDiff[i]))+'%', (x_range[i],.2), ha='center', color='grey', size = annotation_font_size) 
+            else:
+                plt.annotate(str(round(meanDataDS[i]))+'%', (x_range[i],meanDataDS[i]+.5), ha='center', color='grey', size = annotation_font_size)
+            
 #Main plotting function
-def plot_main(targets=[], center_toggle=True, filename_prefix='', interactive=True, save_name=None, x_max = 20, electrodes=1):
+def plot_main(targets=[], base_target=' ', ref_target=' ', center_toggle=True, filename_prefix='', interactive=True, save_name=None, x_max = 20, electrodes=1):
     
     ###############################################
     ## SETUP                                     ##
@@ -341,17 +400,14 @@ def plot_main(targets=[], center_toggle=True, filename_prefix='', interactive=Tr
         #Add difference subplot if two+ targets are selected
         if interactive:
             ax1 = plt.subplot2grid((5, 4), (dat_index, 3), colspan=1)
-            if len(targets) == 1:
-                plotDiffData(targets[0], None, analysis_names, filenames)
-            elif len(targets) >= 2:
-                plotDiffData(targets[0], targets[1], analysis_names, filenames)
+            plotDiffData(ref_target, base_target, analysis_names, filenames)
             ax1.spines[['right', 'top']].set_visible(False)
             x_range = np.arange(1,5) if x_max == 20 else np.arange(1,8)
             x_ticks = ['5','10','15','20'] if x_max == 20 else ['5','10','15','20','30','60','100']
             plt.xlim(0,len(x_ticks)+1)
             plt.xticks(x_range, x_ticks, fontsize=fontsize-2)
 
-            if len(targets) >= 2:
+            if base_target != ' ' and ref_target != ' ':
                 plt.ylim(0,20)
                 plt.yticks(np.arange(0,20,5), fontsize=fontsize-2)
             else:
@@ -359,12 +415,14 @@ def plot_main(targets=[], center_toggle=True, filename_prefix='', interactive=Tr
                 plt.yticks(np.arange(50,ylims,5), fontsize=fontsize-2)
 
             if dat == 1:
-                if len(targets) == 1:
-                    ax1.annotate(f'{targets[0]}',(0.55,ylims), fontsize = fontsize)
-                elif len(targets) >= 2:
-                    ax1.annotate(f"{targets[1].replace('-Augmented','')} - {targets[0].replace('-Augmented','')}",(0.55,15), fontsize = fontsize)
+                if base_target != ' ' and ref_target != ' ':
+                    ax1.annotate(f"{base_target.replace('-Augmented','')} - {ref_target.replace('-Augmented','')}",(0.55,15), fontsize = fontsize)
+                elif base_target != ' ':
+                    ax1.annotate(f'{base_target}',(0.55,ylims), fontsize = fontsize)
+                elif ref_target != ' ':
+                    ax1.annotate(f'{ref_target}',(0.55,ylims), fontsize = fontsize)
                 else:
-                    ax1.annotate(f'Select an Analysis',(0.55,ylims), fontsize = fontsize)
+                    ax1.annotate('Select a comparison',(0.55,ylims), fontsize = fontsize)
             elif dat == 5:
                 plt.xlabel('Sample Size', fontsize=fontsize)
 
