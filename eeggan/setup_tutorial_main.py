10ebd3ef51a788b76ec641bd786873e5b15f7f46
Chad C Williams <cwilliams.uca@gmail.com>
2024-05-29 08:21:14 -0700

    Added placeholders

--- eeggan/setup_tutorial_main.py
+++ eeggan/setup_tutorial_main.py
@@ -16,22 +16,42 @@ def main():
         #Save file to directory
         open(path+url.split('/')[-1], 'wb').write(r.content)
     
-    #Download each file
+    #Download EEG Training Data
     url = 'https://raw.githubusercontent.com/AutoResearch/EEG-GAN/dev/eeggan/data/eeggan_training_example.csv'
     path = 'data/'
     download_file(path, url)
     
+    #Download EEG Validation Data
     url = 'https://raw.githubusercontent.com/AutoResearch/EEG-GAN/dev/eeggan/data/eeggan_validation_example.csv'
     path = 'data/'
     download_file(path, url)
 
-    #url = 'https://raw.githubusercontent.com/AutoResearch/EEG-GAN/main/generated_samples/gansEEGSyntheticData.csv'
-    #path = 'generated_samples/'
+    #Download EEG AE Model
+    #url = 'https://github.com/AutoResearch/EEG-GAN/raw/main/trained_models/aeEEGModel.pt'
+    #path = 'trained_ae/'
     #download_file(path, url)
-    
+
+    #Download EEG GAN Model
     #url = 'https://github.com/AutoResearch/EEG-GAN/raw/main/trained_models/gansEEGModel.pt'
     #path = 'trained_models/'
     #download_file(path, url)
+
+    #Download EEG GAN Synthetic Data
+    #url = 'https://raw.githubusercontent.com/AutoResearch/EEG-GAN/main/generated_samples/gansEEGSyntheticData.csv'
+    #path = 'generated_samples/'
+    #download_file(path, url)
+
+    #Download EEG VAE Model
+    #url = 'https://github.com/AutoResearch/EEG-GAN/raw/main/trained_models/vaeEEGModel.pt'
+    #path = 'trained_vae/'
+    #download_file(path, url)
+
+    #Download EEG VAE Synthetic Data
+    #url = 'https://raw.githubusercontent.com/AutoResearch/EEG-GAN/main/generated_samples/vaeEEGSyntheticData.csv'
+    #path = 'generated_samples/'
+    #download_file(path, url)
+
+    
 
 if __name__ == '__main__':
     main()
