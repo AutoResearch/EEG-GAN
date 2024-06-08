db6e4685621af69f3a67c965964e9deb2e98cf5f
Chad C Williams <cwilliams.uca@gmail.com>
2024-06-08 12:59:29 -0700

    Removed old eeggan package
    This branch will be merged into main as the documentation wrapper, so it does not need the package itself.

--- eeggan/__init__.py
+++
@@ -1,4 +0,0 @@
-from eeggan.Train_Gan import train_gan
-from eeggan.Visualize_Gan import visualize_gan
-from eeggan.Generate_Samples import generate_samples
-from eeggan.Setup_Tutorial import setup_tutorial
